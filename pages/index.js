daabde7c951048eb89d76aab8152cf648ff1589a
warlley01 <warlleyjhones@gmail.com>
2021-01-27 14:21:16 -0500

    2_update

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,12 @@ import db from '../db.json'
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import Head from 'next/head'
+import Input from '../src/components/Input'
+import Button from '../src/components/Button'
 
 //const BackgroundImage = styled.div`
 //  background-image: url(${db.bg});
@@ -17,16 +20,6 @@ import Head from 'next/head'
 
 //`;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -50,17 +43,15 @@ export default function Home() {
               console.log('Fazendo uma submissão por meio do react')
             }}
             >
-              <input
-                onChange={function(infosDoEvento){
-                  console.log(infosDoEvento.target.value)
-                  setName(infosDoEvento.target.value)
-                }}
-                placeholder="Diz ai seu nome"
+              <Input
+                  name="nomeDoUsuario"
+                  onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}                  
+                  placeholder="Diz ai seu nome"
+                  value={name}
                 />
-                <button type="submit" disabled={name.length === 0}>
-                  Jogar
-                  {name}
-                </button>
+                <Button type="submit" disabled={name.length === 0}>
+                  {`Jogar ${name}`}
+                </Button>
             </form>
 
           </Widget.Content>
